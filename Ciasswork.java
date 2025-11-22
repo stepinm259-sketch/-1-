058612db06899dc0b72a8ceb25fe0918fe15b9a4
stepinm259-sketch <stepinm259@gmail.com>
2025-11-22 16:11:29 +0300

    михаил

---
+++ Ciasswork.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Ciasswork {
+
+
+    public static void main(String[] args) {
+        Scanner  scanner =  new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        int sum = 100;
+        for (int i = 1; i  <= n; i++){
+            sum = sum + i;
+
+            System.out.println(sum);
+
+
+        }
+
+
+    }
+
+
+
+
+
+}

798a5f1a7b750f2111dc5ea485af36b8ae43f17b
stepinm259-sketch <stepinm259@gmail.com>
2025-12-02 20:00:57 +0300

    Add files via upload

--- ArraysExample2.java
+++ ArraysExample2.java
@@ -1,30 +1,66 @@
 import java.util.Scanner;
 
-public class ArraysExample2 {
+    class SpiralMatrix {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        scanner.nextInt();
 
-        int n = 4;
-        int m = 6;
-
-
-        int[][] arr = new  int[n][m];
-        for (int  x = 0; x < m;  x++){
-            for (int y = 0; y  < n; y++ ){
-                arr[y][x]  =  + y + x;
+        int N;
+        while (true) {
+            System.out.print("Введите N (1 <= N <= 50): ");
+            N = scanner.nextInt();
+            if (N >= 1 && N <= 50) {
+                break;
+            } else {
+                System.out.println("Ошибка: N должно быть от 1 до 50. Попробуйте ещё раз.");
             }
-
-
         }
-        for (int[] a : arr) {
-            for (int x : a) {
-                System.out.print(x + " ");
+
+
+        int[][] matrix = new int[N][N];
+
+
+        int value = 1;
+        int top = 0;
+        int bottom = N - 1;
+        int left = 0;
+        int right = N - 1;
+
+        while (value <= N * N) {
+
+            for (int col = left; col <= right; col++) {
+                matrix[top][col] = value++;
             }
-            System.out.println(" ");
+            top++;
 
 
+            for (int row = top; row <= bottom; row++) {
+                matrix[row][right] = value++;
+            }
+            right--;
+
+
+            for (int col = right; col >= left; col--) {
+                matrix[bottom][col] = value++;
+            }
+            bottom--;
+
+
+            for (int row = bottom; row >= top; row--) {
+                matrix[row][left] = value++;
+            }
+            left++;
         }
+
+        
+        System.out.println("Матрица " + N + "x" + N + " (спираль):");
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                System.out.printf("%-4d", matrix[i][j]);
+            }
+            System.out.println();
+        }
+
+        scanner.close();
     }
 }

2f1da5431f3ac466dd38cbf3ef1943c14ebcc4cc
stepinm259-sketch <stepinm259@gmail.com>
2025-12-10 12:43:49 +0300

    Add files via upload

---
+++ man.java
@@ -0,0 +1,24 @@
+public class man {
+    public static void main(String[] args) {
+        String str  = new String("Hello");
+
+
+        Player  player = new Player("Artem", 100, "rllllllmm", "red");
+
+        Player  playerTow = new Player("Michael",  "i99999", "red");
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+}
